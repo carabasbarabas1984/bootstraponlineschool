f86eae3817a174abc8fc9614f3b137a05c848070
stas hlazkov <stas.glazkov95@gmail.com>
2018-12-04 21:03:38 +0000

    initial

--- js/index.js
+++ js/index.js
@@ -1,17 +1,1 @@
-let day = ["Неділя","Понеділок","Вівторок","Середа","Четвер","П'ятниця","Субота"];
-let months = ["Січень","Лютий","Березень","Квітень","Травень","Червень","Липень","Серпень","Вересень", "Жовтень", "Листопад", "Грудень"];
-let myData = new Date();
-let fullData = "Сьогодні : " + myData.getDate() + " " + months[myData.getMonth()] + " "+ myData.getFullYear()+", "+ day[myData.getDay()];
-let curenthours = myData.getHours();
-let curentminutes = myData.getMinutes();
-if(curenthours<10){
-    curenthours = "0"+curenthours;
-}
-if(curentminutes<10){
-    curentminutes = "0"+curentminutes;
-}
-let fulltime = "  " + curenthours +" : "+curentminutes;
-
-
-document.querySelector('.data').innerText = fullData;
-document.querySelector('.hours').innerText = fulltime;
+const productList = new NewsList('news.json', $('.products-container'));
---
+++ js/product-list.js
@@ -0,0 +1,48 @@
+class NewsList {
+    constructor(newsUrl, renderContainer, cart) {
+        this.cart = cart;
+        fetch(newsUrl)
+            .then(result => result.json())
+            .then(products => {
+                this.products = products;
+                this.renderProducts(renderContainer, products);
+                this.addEventListeners();
+            })
+    }
+    getProductById(id) {
+        return this.products.find(el => el.id === id);
+    }
+    renderProducts(container, products) {
+        let productListDomString = ''
+        products.forEach(product => {
+            productListDomString +=
+                `<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+                  <div class="card product">
+                    <img class="card-img-top" src="img/siteNews/${product.image}" 
+                        alt="${product.title}">
+                    <div class="card-body">
+                      <h4 class="card-title">${product.title}</h4>
+                      <button class="btn btn-info" data-toggle="modal"
+                        data-target="#productInfoModal" data-id="${product.id}">Читати
+                      </button>
+                    </div>
+                  </div>
+                </div>`;
+        });
+        container.html(productListDomString);
+    }
+    addEventListeners() {
+        $('#productInfoModal').on('show.bs.modal', event => {
+            const button = $(event.relatedTarget); // Button that triggered the modal
+            const id = String(button.data('id')); // Extract info from data-* attributes
+            const product = this.getProductById(id);
+            const modal = $('#productInfoModal');
+            modal.find('.modal-body .card-img-top')
+                .attr('src', 'img/siteNews/' + product.image)
+                .attr('alt', product.title);
+            modal.find('.modal-body .card-title').text(product.title);
+            modal.find('.modal-body .card-text').text(product.description);
+
+        });
+    }
+}
